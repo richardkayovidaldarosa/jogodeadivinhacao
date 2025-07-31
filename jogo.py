342f2f1b2aa0b73885cfe5c987e4319b9eeb0134
richardkayovidaldarosa <richard.vidal.rosa@escola.pr.gov.br>
2025-07-31 12:18:09 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,9 @@
+import random
+print("#######################")
+print("# jogo de adivinhacao #")
+print("#      Richard        #")
+print("#######################")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
